0528859f43ac11e5ec007cc1ca9131336b02b167
acamberos1 <camberos.athian1@gmail.com>
2025-09-26 22:20:44 -0700

    html draft

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -4,4 +4,10 @@ document.querySelector("#qzBtn").addEventListener("click", gradeQuiz);
 function gradeQuiz() {
     let userAnswer1 = document.querySelector('input[name="q1"]:checked').value
     alert(userAnswer1);
+    // let userAnswer2
+    let userAnswer2 = document.querySelector("#q2")
+    //let userAnswer3
+    //let user Answer4
+    // let user Answer5
 }
+// q1 is id from question1 use similar format for q 2 3 4 5
