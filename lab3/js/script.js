7a2dda4627db41ba32946d67d11dbf741043d163
acamberos1 <camberos.athian1@gmail.com>
2025-09-27 18:46:32 -0700

    feedback improved

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,13 +1,61 @@
 document.querySelector("#qzBtn").addEventListener("click", gradeQuiz); 
+// the code above links the html button to run the function defined below
+// const result1 = document.querySelector("#result1");
+// const result2 = document.querySelector("#result2");
+// const result3 = document.querySelector("#result3");
+// const result4 = document.querySelector("#result4");
+// const result5 = document.querySelector("#result5");
+
+
+ function isFormValid()
+{
+    const feedback =document.querySelector("#validateFB")
+    let isValid = true;
+     const q1Checked = document.querySelector('input[name="q1"]:checked');
+    if(!q1Checked)
+    {
+        isValid = false;
+        feedback.textContent = "Question one unanswered";
+    }
+    return isValid
+    // returns is valid status after if statement runs if false/true
+
+}
+
+
+
+
+
+
+
 
 
 function gradeQuiz() {
+
+
+    document.querySelector("#validateFB").innerHTML ="";
+    // resets feedback
+    if(!isFormValid())
+    {
+        return
+    }
+  
     let userAnswer1 = document.querySelector('input[name="q1"]:checked').value
     alert(userAnswer1);
+    console.log(userAnswer1)
+     
+  
     // let userAnswer2
-    let userAnswer2 = document.querySelector("#q2")
+    let userAnswer2 = document.querySelector("#q2").value
+    alert(userAnswer2)
     //let userAnswer3
+    let userAnswer3 = document.querySelector("#q3").value
+    alert(userAnswer3)
     //let user Answer4
-    // let user Answer5
+    // let userAnswer4 = document.querySelector("#q4").value
+    //    alert(userAnswer4)
+    // // let user Answer5
+    // let userAnswer5 = document.querySelector("#q5")
+    //    alert(userAnswer5)
 }
 // q1 is id from question1 use similar format for q 2 3 4 5
